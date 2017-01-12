d480944fef7d47c63c7708a7ead0d90e306f8f3b
clementbmn <clement.baumann@epitech.eu>
2017-01-12 18:18:44 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+/*
+** my_get_next_line.h for my_get_next_line in /home/clementbmn/Documents/Epitech/Elementary_Programming_in_C/CPE_2016_getnextline
+** 
+** Made by Clement
+** Login   <clement.baumann@epitech.eu@epitech.net>
+** 
+** Started on  Thu Jan  5 18:06:48 2017 Clement
+** Last update Thu Jan 12 18:40:39 2017 Clement
+*/
+
+# ifndef READ_SIZE
+
+# define READ_SIZE 10
+
+# endif /* READ_SIZE */
